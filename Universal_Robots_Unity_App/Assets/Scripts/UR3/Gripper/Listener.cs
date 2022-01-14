b9e8a295c2d9c6d88b6e919eb4e77906af1db5ed
murcoder <chris.murauer@yahoo.de>
2022-01-14 10:52:01 +0100

    test

--- Universal_Robots_Unity_App/Assets/Scripts/UR3/Gripper/Listener.cs
+++ Universal_Robots_Unity_App/Assets/Scripts/UR3/Gripper/Listener.cs
@@ -7,8 +7,8 @@ namespace UR3.Gripper
     public class Listener : MonoBehaviour
     {
         private GameObject Robot;
-        [SerializeField] private GameObject gripper;
-        [SerializeField] private Transform playground;
+        [SerializeField] private GameObject _gripper;
+        [SerializeField] private Transform _playground;
 
         // Start is called before the first frame update
         void Start()
@@ -18,20 +18,26 @@ namespace UR3.Gripper
 
         void OnTriggerEnter(Collider collider)
         {
-            Debug.Log("COLLISION ENTER");
+            Debug.Log("COLLISION ENTER " + _gripper.GetComponent<GripperHandler>().IsClosed);
             Robot.GetComponent<InputController>().SetCollisionDetected(true);
-            ur_data_processing.UR_Control_Data.shouldMove = false;
-            collider.attachedRigidbody.isKinematic = true;
-            collider.transform.parent = gripper.transform;
-            
+
+            if (_gripper.GetComponent<GripperHandler>().IsClosed)
+            {
+                collider.attachedRigidbody.isKinematic = true;
+                collider.transform.parent = _gripper.transform;
+            }
+
         }
         
         void OnTriggerExit(Collider collider)
         {
-            Debug.Log("COLLISION LEAVE");
+            Debug.Log("COLLISION EXIT " + _gripper.GetComponent<GripperHandler>().IsClosed);
             Robot.GetComponent<InputController>().SetCollisionDetected(false);
-            collider.attachedRigidbody.isKinematic = false;
-            collider.transform.parent = playground;
+            if (!_gripper.GetComponent<GripperHandler>().IsClosed)
+            {
+                collider.attachedRigidbody.isKinematic = false;
+                collider.transform.parent = _playground;
+            }
         }
     }
 }
